03d80f27016e94637baabc273eafaee991dd8539
Sascha Konietzke <sk@funkfeuer.net>
2016-11-16 20:43:34 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Contentful Deploy demo
+
+This is a simple repository with a build script that will insert the value of a
+CONTENTFUL_TOKEN environment variable.
+
+The [netlify.toml](netlify.toml) file for the repo configures some template settings
+that makes this repo usable with a netlify deploy button like the one below:
+
+[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://deploy-preview-374--app.netlify.com/start/deploy?repository=https://github.com/SaschaKonietzke/contentful-deploy-demo-1)
---
+++ config.rb
@@ -0,0 +1,49 @@
+set :build_dir, 'dist'
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+end
+
+activate :contentful do |f|
+  f.space         = {space: ENV['CONTENTFUL_SPACE']}
+  f.access_token  = ENV['CONTENTFUL_CDA_TOKEN']
+  f.cda_query     = {}
+  f.content_types = {post: 'post'}
+end
+
